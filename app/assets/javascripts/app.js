1ac2357e97be4c0c641c9b34ff88c2dbc23e2c0f
Jacob Sievers <jacosiev@gmail.com>
2015-02-26 16:31:36 +0100

    Added routing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['templates','ngRoute'])
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/test', {
+        templateUrl: 'test.html'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-route
+//= require angular-rails-templates
 //= require_tree .
+//= require_tree ./templates
