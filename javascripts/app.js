a3461fcb0e3e533b2e24f432a5c2c8b533765d54
Jacob Sievers <jacosiev@gmail.com>
2015-03-11 17:17:00 +0100

    Refactored new_index.html into templates

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,13 @@
-var app = angular.module('app', ['ngRoute'])
+var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/test', {
-        templateUrl: 'templates/test.html'
+      when('/', {
+        templateUrl: 'templates/welcome.html'
+      }).
+      when('/exercises/add', {
+        templateUrl: 'templates/add_exercise.html'
       }).
       otherwise({
         redirectTo: '/'
